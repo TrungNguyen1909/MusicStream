TrungNguyen1909/MusicStream

commit 1abf783730fa9a6570fa63fee47c9516156d58d4

Refactor code

server/httpHandler.go
@@ -54,15 +54,15 @@ func (s *Server) audioHandler(c echo.Context) (err error) {
 		isRanged := len(r.Header.Get("Range")) > 0
 		if isRanged {
 			w.WriteHeader(200)
-			w.Write(s.mp3Header)
+			_, _ = w.Write(s.mp3Header)
 			return
 		}
-		w.Write(s.mp3Header)
+		_, _ = w.Write(s.mp3Header)
 		isMP3Stream = true
 		bufferChannel = s.mp3Channel
 	} else {
 		w.Header().Set("Content-Type", "audio/ogg")
-		w.Write(s.oggHeader)
+		_, _ = w.Write(s.oggHeader)
 		bufferChannel = s.vorbisChannel
 	}
 	if isMP3Stream {
@@ -82,7 +82,7 @@ func (s *Server) audioHandler(c echo.Context) (err error) {
 	bufferChannel[0] <- channel
 	bufferChannel[1] <- channel
 	w.Flush()
-	for {
+	for err != nil {
 		select {
 		case <-notify:
 			return
@@ -97,12 +97,10 @@ func (s *Server) audioHandler(c echo.Context) (err error) {
 				log.Println("[", r.URL.Path, "]", "[WARN] chunks from ", chunkID+1, " to ", Chunk.chunkID-1, " have been lost.")
 			}
 			chunkID = Chunk.chunkID
-			_, err := w.Write(Chunk.buffer)
-			if err != nil {
-				break
-			}
+			_, err = w.Write(Chunk.buffer)
 		}
 	}
+	return
 }
 
 func (s *Server) wsHandler(c echo.Context) (err error) {
@@ -115,11 +113,12 @@ func (s *Server) wsHandler(c echo.Context) (err error) {
 	s.connections.Store(ws, ws)
 	defer ws.Close()
 	defer s.connections.Delete(ws)
-	ws.WriteMessage(websocket.TextMessage, s.getPlaying().EncodeJSON())
-	ws.WriteMessage(websocket.TextMessage, s.getQueue().EncodeJSON())
-	for {
+	_ = ws.WriteMessage(websocket.TextMessage, s.getPlaying().EncodeJSON())
+	_ = ws.WriteMessage(websocket.TextMessage, s.getQueue().EncodeJSON())
+	for err != nil {
 		var msg wsMessage
-		_, msgbuf, err := ws.ReadMessage()
+		var msgbuf []byte
+		_, msgbuf, err = ws.ReadMessage()
 		if err != nil {
 			break
 		}
@@ -129,19 +128,19 @@ func (s *Server) wsHandler(c echo.Context) (err error) {
 		}
 		switch msg.Operation {
 		case opSetClientsTrack:
-			ws.WriteMessage(websocket.TextMessage, s.getPlaying().EncodeJSON())
+			err = ws.WriteMessage(websocket.TextMessage, s.getPlaying().EncodeJSON())
 		case opClientRequestTrack:
-			ws.WriteMessage(websocket.TextMessage, s.enqueue(msg).EncodeJSON())
+			err = ws.WriteMessage(websocket.TextMessage, s.enqueue(msg).EncodeJSON())
 		case opClientRequestSkip:
-			ws.WriteMessage(websocket.TextMessage, s.skip().EncodeJSON())
+			err = ws.WriteMessage(websocket.TextMessage, s.skip().EncodeJSON())
 		case opSetClientsListeners:
-			ws.WriteMessage(websocket.TextMessage, s.getListenersCount().EncodeJSON())
+			err = ws.WriteMessage(websocket.TextMessage, s.getListenersCount().EncodeJSON())
 		case opClientRemoveTrack:
-			ws.WriteMessage(websocket.TextMessage, s.removeTrack(msg).EncodeJSON())
+			err = ws.WriteMessage(websocket.TextMessage, s.removeTrack(msg).EncodeJSON())
 		case opClientRequestQueue:
-			ws.WriteMessage(websocket.TextMessage, s.getQueue().EncodeJSON())
+			err = ws.WriteMessage(websocket.TextMessage, s.getQueue().EncodeJSON())
 		case opWebSocketKeepAlive:
-			ws.WriteMessage(websocket.TextMessage, Response{
+			err = ws.WriteMessage(websocket.TextMessage, Response{
 				Operation: opWebSocketKeepAlive,
 				Success:   true,
 			}.EncodeJSON())
@@ -155,7 +154,7 @@ func (s *Server) playingHandler(c echo.Context) (err error) {
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate, public, max-age=0")
 	w.Header().Set("Pragma", "no-cache")
 	w.Header().Set("Expires", "0")
-	w.Write(s.getPlaying().EncodeJSON())
+	_, _ = w.Write(s.getPlaying().EncodeJSON())
 	return
 }
 
@@ -164,7 +163,7 @@ func (s *Server) listenersHandler(c echo.Context) (err error) {
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate, public, max-age=0")
 	w.Header().Set("Pragma", "no-cache")
 	w.Header().Set("Expires", "0")
-	w.Write(s.getListenersCount().EncodeJSON())
+	_, _ = w.Write(s.getListenersCount().EncodeJSON())
 	return
 }
 
@@ -183,7 +182,7 @@ func (s *Server) enqueueHandler(c echo.Context) (err error) {
 			Reason:    "Invalid Query!",
 		})
 	}
-	w.Write(s.enqueue(msg).EncodeJSON())
+	_, _ = w.Write(s.enqueue(msg).EncodeJSON())
 	return
 }
 func (s *Server) skipHandler(c echo.Context) (err error) {
@@ -191,7 +190,7 @@ func (s *Server) skipHandler(c echo.Context) (err error) {
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate, public, max-age=0")
 	w.Header().Set("Pragma", "no-cache")
 	w.Header().Set("Expires", "0")
-	w.Write(s.skip().EncodeJSON())
+	_, _ = w.Write(s.skip().EncodeJSON())
 	return
 }
 func (s *Server) queueHandler(c echo.Context) (err error) {
@@ -199,7 +198,7 @@ func (s *Server) queueHandler(c echo.Context) (err error) {
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate, public, max-age=0")
 	w.Header().Set("Pragma", "no-cache")
 	w.Header().Set("Expires", "0")
-	w.Write(s.getQueue().EncodeJSON())
+	_, _ = w.Write(s.getQueue().EncodeJSON())
 	return
 }
 func (s *Server) removeTrackHandler(c echo.Context) (err error) {
@@ -217,7 +216,7 @@ func (s *Server) removeTrackHandler(c echo.Context) (err error) {
 			Reason:    "Bad Request",
 		})
 	}
-	w.Write(s.removeTrack(msg).EncodeJSON())
+	_, _ = w.Write(s.removeTrack(msg).EncodeJSON())
 	return
 }
 
@@ -234,10 +233,10 @@ func (s *Server) HandleError(err error, c echo.Context) {
 
 	if e, ok := err.(*echo.HTTPError); ok {
 		// Just handle it gracefully
-		c.JSON(e.Code, e.Message)
+		_ = c.JSON(e.Code, e.Message)
 	} else {
 		// internal error: dump it.
-		c.JSON(http.StatusInternalServerError, errCtx{Code: http.StatusInternalServerError})
+		_ = c.JSON(http.StatusInternalServerError, errCtx{Code: http.StatusInternalServerError})
 
 		errStr := fmt.Sprintf("An unexpected error has occured: %v\n", err)
 		path := filepath.Join(os.TempDir(), fmt.Sprintf("MusicStream-%v.txt", time.Now().Format(time.RFC3339)))

common/common.go
@@ -116,7 +116,7 @@ const alphabet string = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789
 //GenerateID generates an unique alphanumberic string
 func GenerateID() (id string) {
 	b := make([]byte, 8)
-	rand.Read(b)
+	_, _ = rand.Read(b)
 	for _, v := range b {
 		id += string(alphabet[int(v)%len(alphabet)])
 	}

csn/csn.go
@@ -294,6 +294,6 @@ func (track *Track) Populate() (err error) {
 func NewClient() (*Client, error) {
 	cookiesJar, _ := cookiejar.New(nil)
 	client := &http.Client{Jar: cookiesJar, Timeout: 9 * time.Second}
-	pattern, _ := regexp.Compile("sources: \\[([^\\]]*)\\]")
+	pattern, _ := regexp.Compile(`sources: \[([^\]]*)\]`)
 	return &Client{HTTPClient: client, pattern: pattern}, nil
 }

deezer/deezer.go
@@ -41,7 +41,7 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/TrungNguyen1909/MusicStream/common"
-
+	//lint:ignore SA1019 Deezer uses blowfish, who cares?
 	"golang.org/x/crypto/blowfish"
 	"golang.org/x/text/encoding/charmap"
 )
@@ -282,7 +282,6 @@ type trackDecrypter struct {
 	r           io.ReadCloser
 	BlowfishKey []byte
 	counter     int
-	byteCounter int
 	buffer      bytes.Buffer
 	ended       bool
 }
@@ -472,6 +471,7 @@ func (client *Client) PopulateMetadata(dTrack *Track) (err error) {
 	if len(dTrack.deezerTrack.MD5Origin) <= 0 {
 		if client == nil {
 			err = errors.WithStack(errors.New("nil Deezer Client"))
+			return
 		}
 		tracks := make([]deezerTrack, 1)
 		tracks[0] = dTrack.deezerTrack

mxmlyrics/mxmlyrics.go
@@ -189,8 +189,7 @@ func (client *Client) GetLyrics(track, artist, album, artists, ISRC, SpotifyURI 
 		result.SyncedLyrics = syncedLyrics
 	}
 	var originalSyncedLyrics []common.LyricsLine
-	err = json.Unmarshal(([]byte)(sd), &originalSyncedLyrics)
-	err = nil
+	_ = json.Unmarshal(([]byte)(sd), &originalSyncedLyrics)
 	if len(result.SyncedLyrics) == 0 {
 		result.SyncedLyrics = originalSyncedLyrics
 	} else if len(result.SyncedLyrics) == len(originalSyncedLyrics) {

server/distributor.go
@@ -114,12 +114,12 @@ func (s *Server) streamMP3(encodedDuration chan time.Duration) chan *chunk {
 			var pcm []byte
 			if Chunk.buffer != nil {
 				pcm = make([]byte, (1152*4)*(buffer.Len()/(1152*4)))
-				buffer.Read(pcm)
+				_, _ = buffer.Read(pcm)
 			} else {
 				sz := buffer.Len()
 				sz += (1152*4 - sz%(1152*4))
 				pcm = make([]byte, sz)
-				buffer.Read(pcm)
+				_, _ = buffer.Read(pcm)
 			}
 			output := make([]byte, 20000)
 			n := s.mp3Encoder.Encode(output, pcm)
@@ -201,10 +201,8 @@ func (s *Server) streamToClients(quit chan int, quitPreload chan int) time.Time 
 				}
 				quit = nil
 			case vorbisTime = <-quitVorbis:
-				select {
-				case mp3Time = <-quitMP3:
-					interrupted = true
-				}
+				mp3Time = <-quitMP3
+				interrupted = true
 			}
 		}
 	} else {
@@ -243,7 +241,7 @@ func (s *Server) setListenerCount() {
 func (s *Server) webSocketAnnounce(msg []byte) {
 	s.connections.Range(func(key, value interface{}) bool {
 		ws := value.(*webSocket)
-		ws.WriteMessage(websocket.TextMessage, msg)
+		_ = ws.WriteMessage(websocket.TextMessage, msg)
 		return true
 	})
 }

server/request.go
@@ -137,19 +137,13 @@ func (s *Server) getQueue() Response {
 func (s *Server) removeTrack(msg wsMessage) Response {
 	removed := s.playQueue.Remove(func(value interface{}) bool {
 		ele := value.(common.Track)
-		if ele.PlayID() == msg.Query {
-			return true
-		}
-		return false
+		return ele.PlayID() == msg.Query
 	})
 	var removedTrack common.TrackMetadata
 	if removed != nil {
 		removedTrack = s.cacheQueue.Remove(func(value interface{}) bool {
 			ele := value.(common.TrackMetadata)
-			if ele.PlayID == msg.Query {
-				return true
-			}
-			return false
+			return ele.PlayID == msg.Query
 		}).(common.TrackMetadata)
 	}
 	resp := Response{

server/server.go
@@ -49,7 +49,6 @@ import (
 )
 
 const (
-	chunkDelayMS          = 50
 	opSetClientsTrack     = 1
 	opAllClientsSkip      = 2
 	opClientRequestTrack  = 3
@@ -99,7 +98,6 @@ type Server struct {
 	startTime            time.Time
 	cacheQueue           *queue.Queue
 	streamMux            sync.Mutex
-	encoderWg            sync.WaitGroup
 	minifier             *minify.M
 	activityWg           sync.WaitGroup
 	newListenerC         chan int

commit ba25292b0bb0cc338e3b5a140deafeb7bc43412b

Logic bugs introduced by 1abf783

server/httpHandler.go
@@ -32,6 +32,7 @@ import (
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 )
 
 func (s *Server) audioHandler(c echo.Context) (err error) {
@@ -82,7 +83,7 @@ func (s *Server) audioHandler(c echo.Context) (err error) {
 	bufferChannel[0] <- channel
 	bufferChannel[1] <- channel
 	w.Flush()
-	for err != nil {
+	for err == nil {
 		select {
 		case <-notify:
 			return
@@ -115,7 +116,7 @@ func (s *Server) wsHandler(c echo.Context) (err error) {
 	defer s.connections.Delete(ws)
 	_ = ws.WriteMessage(websocket.TextMessage, s.getPlaying().EncodeJSON())
 	_ = ws.WriteMessage(websocket.TextMessage, s.getQueue().EncodeJSON())
-	for err != nil {
+	for err == nil {
 		var msg wsMessage
 		var msgbuf []byte
 		_, msgbuf, err = ws.ReadMessage()
@@ -145,6 +146,11 @@ func (s *Server) wsHandler(c echo.Context) (err error) {
 				Success:   true,
 			}.EncodeJSON())
 		}
+	}
+	if !websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+		err = nil
+	} else {
+		err = errors.WithStack(err)
 	}
 	return
 }